74311b4e14e132d612354e0be02545585f441379
teresacabell <teresacabll@gmail.com>
2022-02-16 17:05:35 -0600

    working on comment routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +10,23 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
