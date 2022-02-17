aa05c5f9b50d9dea0368652985b3fb38f50d6e48
teresacabell <teresacabll@gmail.com>
2022-02-17 12:17:52 -0600

    homepage not working

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const route = require('express').Router();
 
 const apiRoutes = require('./api');
 const router = require('./api/user-routes');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(()=> {
