6d6e450d21af7229bdaf7187ddef9c1a1fab4366
teresacabell <teresacabll@gmail.com>
2022-02-17 12:57:49 -0600

    reversed

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
+
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(()=> {
     app.listen(PORT, () => console.log('Now listening'));
