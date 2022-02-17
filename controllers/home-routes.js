1a96c8472c5374b6d4accda9ce9faababcc3bb7d
teresacabell <teresacabll@gmail.com>
2022-02-17 13:15:10 -0600

    fixed homepage render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,41 @@
 const router = require("express").Router();
+const sequelize = require('../config/connection');
 
-router.get("/", (req, res) => {
-  res.render("homepage");
+const { Post, User, Comment } =require('../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
